fd0dba05efce6d1b1235388a05498fa016a5cfa1
Carl Edmunds <carledmunds90@googlemail.com>
2018-05-03 11:09:19 +0100

    Trying the LdapContext with no env settings

--- ispyb-ws/src/main/java/ispyb/ws/rest/security/login/ESRFLoginModule.java
+++ ispyb-ws/src/main/java/ispyb/ws/rest/security/login/ESRFLoginModule.java
@@ -68,7 +68,10 @@ public class ESRFLoginModule {
 
 		if (!password.isEmpty()){
 			System.out.printf("			Attempt to create InitialLdapContext\n");
-			InitialLdapContext ctx = new InitialLdapContext(getConnectionProperties(username, password), null);
+//			InitialLdapContext ctx = new InitialLdapContext(getConnectionProperties(username, password), null);
+			InitialLdapContext ctx = new InitialLdapContext(null, null);
+
+			System.out.println("			Got past the InitialLdapContext bit... ");
 
 			// Set up search constraints
 			SearchControls cons = new SearchControls();
