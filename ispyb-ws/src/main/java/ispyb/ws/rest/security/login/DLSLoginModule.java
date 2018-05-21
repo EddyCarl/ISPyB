1a0c3ff2b448bb91d050503e81f6df6f3342e961
Carl Edmunds <carledmunds90@googlemail.com>
2018-05-21 11:14:41 +0100

    Removing unnecessary code from the dummy module

--- ispyb-ws/src/main/java/ispyb/ws/rest/security/login/DLSLoginModule.java
+++ ispyb-ws/src/main/java/ispyb/ws/rest/security/login/DLSLoginModule.java
@@ -45,17 +45,45 @@ public class DLSLoginModule
    * @return Properties props
    *        - A set of environment variables to configure the DLSLoginModule
    */
-  private Properties getConnectionProperties(final String username, final String password)
-  {
-    Properties props = new Properties();
+//  private Properties getConnectionProperties(final String username, final String password)
+//  {
+//    Properties props = new Properties();
+//
+//    props.put( "java.naming.provider.url", SERVER_URL );
+//    props.put("java.naming.security.authentication", AUTH_TYPE);
+//
+//
+//
+//
+//
+////    env.put( "java.naming.factory.initial", "com.sun.jndi.ldap.LdapCtxFactory" );
+////    env.put( "principalDNPrefix", principalDNPrefix );
+////    env.put( "java.naming.security.principal", "uid=" + username + ",ou=People,dc=esrf,dc=fr" );
+////    env.put( "groupAttributeID", groupAttributeID );
+////    env.put( "groupCtxDN", groupCtxDN );
+////    env.put( "principalDNSuffix", principalDNSuffix );
+////    env.put( "allowEmptyPasswords", "false" );
+////    env.put( "groupUniqueMember", groupUniqueMemberName );
+////    env.put( "jboss.security.security_domain", "ispyb" );
+////    env.put( "java.naming.provider.url", server );
+////    env.put( "java.naming.security.credentials", password );
+//
+//
+//
+//    return props;
+//  }
 
-    props.put( "java.naming.provider.url", SERVER_URL );
-    props.put("java.naming.security.authentication", AUTH_TYPE);
-
-
-
-
-
+//  protected static Properties getConnectionProperties( String username, String password )
+//  {
+//    private static String groupUniqueMemberName = "uniqueMember";
+//    private static String principalDNSuffix = ",ou=People,dc=esrf,dc=fr";
+//    private static String groupCtxDN = "ou=Pxwebgroups,dc=esrf,dc=fr";
+//    private static String principalDNPrefix = "uid=";
+//    private static String groupAttributeID = "cn";
+//    private static String server = "ldap://ldap.esrf.fr:389/";
+//
+//
+//    Properties env = new Properties();
 //    env.put( "java.naming.factory.initial", "com.sun.jndi.ldap.LdapCtxFactory" );
 //    env.put( "principalDNPrefix", principalDNPrefix );
 //    env.put( "java.naming.security.principal", "uid=" + username + ",ou=People,dc=esrf,dc=fr" );
@@ -66,41 +94,13 @@ public class DLSLoginModule
 //    env.put( "groupUniqueMember", groupUniqueMemberName );
 //    env.put( "jboss.security.security_domain", "ispyb" );
 //    env.put( "java.naming.provider.url", server );
+//    env.put( "java.naming.security.authentication", "simple" );
 //    env.put( "java.naming.security.credentials", password );
-
-
-
-    return props;
-  }
-
-  protected static Properties getConnectionProperties( String username, String password )
-  {
-    private static String groupUniqueMemberName = "uniqueMember";
-    private static String principalDNSuffix = ",ou=People,dc=esrf,dc=fr";
-    private static String groupCtxDN = "ou=Pxwebgroups,dc=esrf,dc=fr";
-    private static String principalDNPrefix = "uid=";
-    private static String groupAttributeID = "cn";
-    private static String server = "ldap://ldap.esrf.fr:389/";
-
-
-    Properties env = new Properties();
-    env.put( "java.naming.factory.initial", "com.sun.jndi.ldap.LdapCtxFactory" );
-    env.put( "principalDNPrefix", principalDNPrefix );
-    env.put( "java.naming.security.principal", "uid=" + username + ",ou=People,dc=esrf,dc=fr" );
-    env.put( "groupAttributeID", groupAttributeID );
-    env.put( "groupCtxDN", groupCtxDN );
-    env.put( "principalDNSuffix", principalDNSuffix );
-    env.put( "allowEmptyPasswords", "false" );
-    env.put( "groupUniqueMember", groupUniqueMemberName );
-    env.put( "jboss.security.security_domain", "ispyb" );
-    env.put( "java.naming.provider.url", server );
-    env.put( "java.naming.security.authentication", "simple" );
-    env.put( "java.naming.security.credentials", password );
-
-    System.out.printf( "			Returning the ESRFLoginModule properties correctly\n" );
-
-    return env;
-  }
+//
+//    System.out.printf( "			Returning the ESRFLoginModule properties correctly\n" );
+//
+//    return env;
+//  }
 
 
   protected static String getFilter( String username )
