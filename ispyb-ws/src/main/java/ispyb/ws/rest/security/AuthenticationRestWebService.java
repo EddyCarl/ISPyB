0ea7cad678fa99fca02ec33918ad2dcc830d9ab0
Carl Edmunds <carledmunds90@googlemail.com>
2018-06-04 11:29:41 +0100

    Exposing endpoints under the '/rest/security' directory to Swagger

--- ispyb-ws/src/main/java/ispyb/ws/rest/security/AuthenticationRestWebService.java
+++ ispyb-ws/src/main/java/ispyb/ws/rest/security/AuthenticationRestWebService.java
@@ -31,6 +31,9 @@ import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 
+import io.swagger.annotations.Api;
+
+@Api
 @Path("/")
 public class AuthenticationRestWebService extends RestWebService {
 	private final static Logger logger = Logger.getLogger(AuthenticationRestWebService.class);
