f0601eb06de94999c9dfdd9541f13875a136f6f3
Carl Edmunds <carledmunds90@googlemail.com>
2018-06-05 12:43:28 +0100

    Adding debug...

--- ispyb-ejb/src/main/java/ispyb/server/mx/services/ws/rest/WsServiceBean.java
+++ ispyb-ejb/src/main/java/ispyb/server/mx/services/ws/rest/WsServiceBean.java
@@ -9,10 +9,14 @@ import org.hibernate.SQLQuery;
 import org.hibernate.transform.AliasToEntityMapResultTransformer;
 
 public class WsServiceBean {
-	
+
 	protected List<Map<String, Object>> executeSQLQuery(SQLQuery query ){
 		query.setResultTransformer(AliasToEntityMapResultTransformer.INSTANCE);
 		@SuppressWarnings("unchecked")
+
+			System.out.println("Query list: " + query.list());
+
+
 		List<Map<String, Object>> aliasToValueMapList = query.list();
 		return aliasToValueMapList;
 	}
@@ -28,6 +32,6 @@ public class WsServiceBean {
 	        IOUtils.closeQuietly(str);
 	    }
 	}
-	    
-	    
+
+
 }
