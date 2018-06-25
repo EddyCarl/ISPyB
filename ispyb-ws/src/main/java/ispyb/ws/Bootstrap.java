6006fd35e015569e1363db2ee35dda703f8529d7
Carl Edmunds <carledmunds90@googlemail.com>
2018-06-25 18:54:02 +0100

    Adding further changes to Bootstrap class

--- ispyb-ws/src/main/java/ispyb/ws/Bootstrap.java
+++ ispyb-ws/src/main/java/ispyb/ws/Bootstrap.java
@@ -4,17 +4,21 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.ws.rs.Path;
 
 import io.swagger.annotations.Api;
 import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.config.SwaggerContextService;
 import io.swagger.models.Info;
+import io.swagger.models.Scheme;
 import io.swagger.models.Swagger;
 import io.swagger.models.Tag;
 import io.swagger.models.auth.ApiKeyAuthDefinition;
 import io.swagger.models.auth.In;
 
-@Api(value = "/ispyb-ws")
+import java.util.Collections;
+
+//@Api(value = "/ispyb-ws")
 public class Bootstrap extends HttpServlet {
 	/**
 	 *
@@ -22,12 +26,14 @@ public class Bootstrap extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
+  @Path( "/ispyb-ws" )
 	public void init(ServletConfig config) throws ServletException {
 //		super.init(config);
 
 		Info info = new Info()
 			.title("Some new test title")
-			.description("Adding some bloat text for test purposes");
+			.description("Adding some bloat text for test purposes")
+      .version( "5.4.3" );
 
 		ServletContext context = config.getServletContext();
 		Swagger swagger = new Swagger().info( info );
@@ -41,10 +47,14 @@ public class Bootstrap extends HttpServlet {
 		.name( "Another test tag" )
 		.description( "More rubbish text" ));
 
+    swagger.setSchemes( Collections.singletonList( Scheme.HTTP ) );
+    swagger.setHost("192.168.30.200:8080");
+    swagger.setBasePath("/ispyb/ispyb-ws/rest");
+//    swagger.setResourcePackage("io.swagger.resources");
+//    swagger.setScan(true);
+
 		new SwaggerContextService().withServletConfig( config ).updateSwagger( swagger );
 
-
-//
 //		BeanConfig beanConfig = new BeanConfig();
 //		beanConfig.setVersion("5.4.3");
 //		beanConfig.setSchemes(new String[] { "http" });
