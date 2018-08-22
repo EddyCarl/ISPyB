71af9f551602039f4c54950c2450168c26c25762
Carl Edmunds <carledmunds90@googlemail.com>
2018-08-22 16:26:21 +0100

    Clean up the token retrieval code

--- ispyb-ws/src/main/java/utils/DLSApiAuthenticationChecker.java
+++ ispyb-ws/src/main/java/utils/DLSApiAuthenticationChecker.java
@@ -3,16 +3,14 @@ package utils;
 import ispyb.server.common.services.login.Login3Service;
 import ispyb.server.common.vos.login.Login3VO;
 
-import javax.naming.NamingException;
 import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MultivaluedMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class DLSApiAuthenticationChecker
 {
   private final static String API_TOKEN_HEADER = "api_token";
+
+
+
 
   public static String retrieveToken( final HttpHeaders headers )
   {
@@ -20,69 +18,10 @@ public class DLSApiAuthenticationChecker
 
     if( headers != null )
     {
-
-      MultivaluedMap<String, String> headerMap = headers.getRequestHeaders();
-
-      if( headerMap == null )
+      if( !headers.getRequestHeader( API_TOKEN_HEADER ).isEmpty() )
       {
-        System.out.println( " === The header map is null === ");
+        apiToken = headers.getRequestHeader( API_TOKEN_HEADER ).get( 0 );
       }
-      if( headerMap.isEmpty() )
-      {
-        System.out.println( " === The header map is empty === ");
-      }
-
-      System.out.println( " === The header map size is: " + headerMap.size() );
-
-
-      for( Map.Entry<String, List<String>> me : headerMap.entrySet() )
-      {
-        String key = me.getKey();
-
-        List<String> values = me.getValue();
-
-
-        System.out.println( "    Key: " + key );
-        System.out.println( "     Values size: " + values.size() );
-
-        for( String value : values )
-        {
-          System.out.println( "             " + value );
-        }
-      }
-
-
-      System.out.println(" ---------- Time for the real stuff ------------- ");
-
-
-      List<String> headerValues = headers.getRequestHeader( API_TOKEN_HEADER );
-      if( headerValues != null )
-      {
-        System.out.println( "headers.getReqHeader for API TOKEN is NOT null ");
-
-
-        // I think we hit an exception when trying to "get(0)" if the API token hasn't actually been input
-        // into the header... Investigate further
-
-        System.out.println("    size of headervalue list: " +  headerValues.size() );
-
-        for( String hv : headerValues )
-        {
-          System.out.println( "       " + hv );
-        }
-
-
-
-        if( headers.getRequestHeader( API_TOKEN_HEADER ).get( 0 ) != null )
-        {
-          apiToken = headers.getRequestHeader( API_TOKEN_HEADER ).get( 0 );
-        }
-      }
-      else
-      {
-        System.out.println("headers.getreqheader for API token is NULL ");
-      }
-
     }
 
     return apiToken;
